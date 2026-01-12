9b65de47fb30dcea01224827a0528ea201b4183f
Petar <petarlukovic17@gmail.com>
2026-01-13 00:28:45 +0100

    Added header file

--- include/ClipboardManager.hpp
+++ include/ClipboardManager.hpp
@@ -2,31 +2,43 @@
 #define ClipboardManager_HPP
 
 #include <bits/c++config.h>
-#include <deque>
+#include <cstddef>
 #include <string>
-#include <mutex>
+#include <pthread.h>
+
+#define SHARED_NAME "/clipboardhistory_shm"
+#define MAX_ITEMS 20
+#define MAX_LENGTH 4096
+
+struct SharedClipboard {
+	pthread_mutex_t mutex;
+	size_t count;
+	char items[MAX_ITEMS][MAX_LENGTH];
+
+};
+
 
 class ClipboardHistory {
 	public:
-		explicit ClipboardHistory(std::size_t _maxSize = 20);
-		void showMenu();
-		void updateClipboard();
+		explicit ClipboardHistory(std::size_t maxSize = MAX_ITEMS);
+		~ClipboardHistory();
 
+		void updateClipboard();
+		void showMenu();
+	
 	private:
-		
 		std::size_t _maxSize;
 		std::string _lastValue;
-		std::mutex _mutex;
 
+		int _shmFd;
+		SharedClipboard* _shm;
+
+		void initSharedMemory(bool create);
 		void addItem(const std::string& text);
+
 		std::string readClipboard() const;
 		void writeClipboard(const std::string& text) const;
-		
-		// use for reading and writing from "tmp" file
-		std::deque<std::string> loadHistory() const;
-		void saveHistory(const std::deque<std::string>& history) const;
-
-};	
+};		
 
 
 #endif
